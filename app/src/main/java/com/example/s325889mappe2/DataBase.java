e1273d36823dc50d70e446ea09acb5f6ccc92abe
even996 <evenolsen96@gmail.com>
2019-10-16 14:30:28 +0200

    LAYout på ting og laga en database

---
+++ app/src/main/java/com/example/s325889mappe2/DataBase.java
@@ -0,0 +1,42 @@
+package com.example.s325889mappe2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBase extends SQLiteOpenHelper {
+
+
+    public static final String DATABASE_NAME = "DATABASE.db";
+    public static final String TABLE_NAME = "Resturant_table";
+    public static final String TABLE_NAME2 = "Friends_table";
+    public static final String TABLE_NAME3 = "Order_table";
+    public static final String REST_COL_1 = "ID";
+    public static final String REST_COL_2 = "NAME";
+    public static final String REST_COL_3 = "ADRESS";
+    public static final String REST_COL_4 = "TELEPHONE";
+    public static final String REST_COL_5 = "TYPE";
+
+
+
+    public DataBase(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (" + REST_COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        REST_COL_2 + " TEXT," + REST_COL_3 + " TEXT, " + REST_COL_4 + " TEXT, "  + REST_COL_5 + " TEXT" + ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+}
